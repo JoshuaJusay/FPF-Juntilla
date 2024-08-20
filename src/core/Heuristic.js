7e2fdb713bf20673d916e9e10e09f30ae25eea8a
John Mark S. Aguilar <johnmark.aguilar111@gmail.com>
2024-08-20 12:05:30 +0800

    Update Heuristic.js

--- src/core/Heuristic.js
+++ src/core/Heuristic.js
@@ -13,38 +13,6 @@ module.exports = {
   manhattan: function(dx, dy) {
       return dx + dy;
   },
-
-  /**
-   * Euclidean distance.
-   * @param {number} dx - Difference in x.
-   * @param {number} dy - Difference in y.
-   * @return {number} sqrt(dx * dx + dy * dy)
-   */
-  euclidean: function(dx, dy) {
-      return Math.sqrt(dx * dx + dy * dy);
-  },
-
-  /**
-   * Octile distance.
-   * @param {number} dx - Difference in x.
-   * @param {number} dy - Difference in y.
-   * @return {number} sqrt(dx * dx + dy * dy) for grids
-   */
-  octile: function(dx, dy) {
-      var F = Math.SQRT2 - 1;
-      return (dx < dy) ? F * dx + dy : F * dy + dx;
-  },
-
-  /**
-   * Chebyshev distance.
-   * @param {number} dx - Difference in x.
-   * @param {number} dy - Difference in y.
-   * @return {number} max(dx, dy)
-   */
-  chebyshev: function(dx, dy) {
-      return Math.max(dx, dy);
-  },
-
   /**
   * Safest-route heuristic.
   * @param {number} WL - Water level at node n.
@@ -56,4 +24,5 @@ module.exports = {
   }
 
 };
-//Added Safest-route Heuristic - subject to changes/temporary
+//Added Safest-route Heuristic - subject to changes/temporary (19/8)
+//Removed unnecessary Heuristics (20/8)
